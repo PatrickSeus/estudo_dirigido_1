c7d49e8c4a39447897dc0bf939850ab1540e6bea
PatrickSeus <49734958+PatrickSeus@users.noreply.github.com>
2019-04-17 20:44:47 -0300

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,5 @@
+distancia_viagem = float(input("distancia percorrida em km: "))
+
+dias_viagem = float(input("quantidade de dias utilizado o carro: "))
+
+print(" Nesta viagem voce utilizou: ", "R$:", dias_viagem * 60, "em", dias_viagem ,"diarias","e R$:",distancia_viagem * 0.15, "no total de: ",distancia_viagem, "km rodados no periodo.")
